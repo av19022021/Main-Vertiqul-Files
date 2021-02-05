44fb077dc9b0f37aca784607c54ac124436c20f9
av19022021 <77366955+av19022021@users.noreply.github.com>
2021-02-05 14:16:32 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-<!-- Ad Blur -->
-<script>
-	var ads = document.getElementsByClassName('ad-blur');
-	var adsArray = Array.prototype.slice.call(ads);
-	var adsBlurCss = ''
-	
-	;(function() {
-	  adsArray.forEach(function(ad) {
-	  	 var imageUrl = ad.childNodes[1].children[0].style.backgroundImage;
-		 var css = "#" + ad.id + " .et_parallax_bg::before{background-image:" + imageUrl + "}";
-		 adsBlurCss += css;
-	  })
-	})()
-	
-	var adBlurStyleTag = document.createElement("style");
-	adBlurStyleTag.innerHTML = adsBlurCss;
-	document.head.appendChild(adBlurStyleTag);
-</script>
-<!-- End Ad Blur -->
+//  Ad Blur
+var ads = document.getElementsByClassName("ad-blur");
+var adsArray = Array.prototype.slice.call(ads);
+var adsBlurCss = "";
+
+(function () {
+  adsArray.forEach(function (ad) {
+    var imageUrl = ad.childNodes[1].children[0].style.backgroundImage;
+    var css = "#" + ad.id + " .et_parallax_bg::before{background-image:" + imageUrl + "}";
+    adsBlurCss += css;
+  });
+})();
+
+var adBlurStyleTag = document.createElement("style");
+adBlurStyleTag.innerHTML = adsBlurCss;
+document.head.appendChild(adBlurStyleTag);
+// End of Adblur
