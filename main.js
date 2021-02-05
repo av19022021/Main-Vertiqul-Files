20913464b004d2da468a5f98f391eb6068e8403e
av19022021 <77366955+av19022021@users.noreply.github.com>
2021-02-05 15:27:55 -0800

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,11 @@ var ads = document.getElementsByClassName("ad-blur");
 var adsArray = Array.prototype.slice.call(ads);
 var adsBlurCss = "";
 
-(function () {
-  adsArray.forEach(function (ad) {
-    var imageUrl = ad.childNodes[1].children[0].style.backgroundImage;
-    var css = "#" + ad.id + " .et_parallax_bg::before{background-image:" + imageUrl + "}";
-    adsBlurCss += css;
-  });
-})();
+adsArray.forEach(function (ad) {
+  var imageUrl = ad.childNodes[1].children[0].style.backgroundImage;
+  var css = "#" + ad.id + " .et_parallax_bg::before{background-image:" + imageUrl + "}";
+  adsBlurCss += css;
+});
 
 var adBlurStyleTag = document.createElement("style");
 adBlurStyleTag.innerHTML = adsBlurCss;
